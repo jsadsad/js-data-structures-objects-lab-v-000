a1cea2b2fb0daad612ee6a51c68437c9325f4c51
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-07-19 01:14:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  
+  const newObj = Ob
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
